78ede8cd9fec80b14d34063ee301d13e005e2dac
Worulim <sinasina427@gmail.com>
2026-01-12 17:46:32 +0900

    정답

--- closure1-1.js
+++ closure1-1.js
@@ -1,14 +1,24 @@
 function createCounter(initialValue = 0) {
-	let value = 0;
-  //TODO: 구현하세요
+  let value = initialValue;
+
+  return {
+    increment: () => (value = value + 1),
+    decrement: () => (value = value - 1),
+    getValue: () => value,
+    reset: () => (value = initialValue),
+  };
 }
 
 // 테스트
 const counter = createCounter(10);
 const counter2 = createCounter(10);
+
 console.log(counter.increment()); // 11
 console.log(counter.increment()); // 12
 console.log(counter.decrement()); // 11
 console.log(counter.getValue());  // 11
 counter.reset();
 console.log(counter.getValue());  // 10
+
+// counter2는 counter랑 완전 별개로 값 관리됨
+console.log(counter2.getValue()); // 10
